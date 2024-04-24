dd57e367b506c86ef253cc30b3ee5ff0769b9174
Arnau <>
2024-04-24 20:08:50 +0200

    chore(gruvbox): add transparent background

--- init.lua
+++ init.lua
@@ -667,6 +667,9 @@ require('lazy').setup({
     -- 'folke/tokyonight.nvim',
     'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      transparent_mode = true,
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
